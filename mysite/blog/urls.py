169f0fe90133a443800b311fea90eac8080e225b
Bieelogrom <gabrielamorim15436@gmail.com>
2025-01-02 20:36:06 -0300

    feat: detalhes da postagem

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+import uuid
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<uuid:pk>/', views.post_detail, name='post_detail')
 ]
