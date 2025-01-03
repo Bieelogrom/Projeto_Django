911c1a1d54781fd4850851be10cf5272ec840dde
Bieelogrom <gabrielamorim15436@gmail.com>
2025-01-03 12:38:25 -0300

    feat: capacidade de postar e editar post

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,5 +4,7 @@ import uuid
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<uuid:pk>/', views.post_detail, name='post_detail')
+    path('post/<uuid:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<uuid:pk>/edit/', views.post_edit, name='post_edit'),
 ]
